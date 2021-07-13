69de74223526e65758dde55bab4db2aa4fe3c5d8
TriDiamond <code.tridiamond@gmail.com>
2021-07-14 01:22:52 +0800

    Boost up performance and Best practice score

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,8 @@ const Card = ({ name, email, id }) => {
   return (
     <div className="flex flex-col items-center p-5 rounded-lg shadow-lg transition-transform hover:scale-110 bg-gray-800 text-gray-400">
       <img
-        className="w-44 h-44"
+        width="200"
+        height="200"
         src={`https://robohash.org/${id}?set=set4&size=200x200`}
         alt="kitty"
       />
